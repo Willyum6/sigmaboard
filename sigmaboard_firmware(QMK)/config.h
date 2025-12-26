20ac8b3f72ef217dfe52de6494a7137492ffd646
Willyum6 <william.e.tran@gmail.com>
2025-12-26 00:14:21 -0600

    Add files via upload

---
+++ sigmaboard_firmware(QMK)/config.h
@@ -0,0 +1,16 @@
+
+/*
+Copyright 2025 William Tran
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+*/
+
+#define WS2812_DI_PIN GP3
+#define RGBLIGHT_LED_COUNT 2
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP7
+#define I2C1_SCL_PIN GP0
